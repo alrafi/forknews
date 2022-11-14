b3871575b7270d1b401991f751d695a8e06c07bc
alrafi <hafisalrafi@gmail.com>
2022-11-14 09:29:41 +0700

    Update package and config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        whitedarker: "#f5f8fa",
+      },
+    },
   },
   plugins: [],
 };
